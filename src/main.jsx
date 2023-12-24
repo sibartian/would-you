d6856e4778c3181fab543d61784800df69fd44b6
Sibartian . <sibogatulinartem9@gmail.com>
2023-12-24 14:09:53 +0300

    routing changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 import {createBrowserRouter,
-  RouterProvider, BrowserRouter, Routes, Route } from "react-router-dom";
+  RouterProvider, HashRouter, Routes, Route } from "react-router-dom";
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App/>
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>,
 )
