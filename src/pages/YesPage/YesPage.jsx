d5296405ec8aa7d95b656a32eb1a0fee1a4a18d3
Sibartian . <sibogatulinartem9@gmail.com>
2023-12-24 15:15:25 +0300

    design changings

--- src/pages/YesPage/YesPage.jsx
+++ src/pages/YesPage/YesPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-
+import './Yes.scss'
 const YesPage = () => {
 	return (
-		<div class="container">
-            <div >
+    <div className='yes__container'>
                 <h1 class = "header_text">I knew it!!</h1>
-            </div>
             <div class="gif_container">
                 <img src="https://media0.giphy.com/media/T86i6yDyOYz7J6dPhf/giphy.gif" alt="Cute animated illustration"/>
             </div>
