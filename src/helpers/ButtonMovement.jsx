48fab11d6005704463256250aa69f07f72349581
Sibartian . <sibogatulinartem9@gmail.com>
2023-12-24 15:34:48 +0300

    button changes

--- src/helpers/ButtonMovement.jsx
+++ src/helpers/ButtonMovement.jsx
@@ -3,19 +3,21 @@ import React from 'react'
 const ButtonMovement = props => {
 
 	const moveButton = (e) =>  {
-		var x = Math.random() * (window.innerWidth - e.offsetWidth)
-		var y = Math.random() * (window.innerHeight - e.offsetHeight)
-		if(x >= window.innerWidth) {
-			e.style.left = `${x - 40}px`
-		} else {
-			e.style.left = `${x}px`
-		}
-		if (y > window.innerHeight) {
-			e.style.top = `${y- 20}px`
-		} else {
-			e.style.top = `${y}px`
-		}
-	
+		// var x = Math.random() * (window.innerWidth - 50 - e.offsetWidth)
+		// var y = Math.random() * (window.innerHeight - 50 - e.offsetHeight)
+		// if(x >= window.innerWidth) {
+		// 	e.style.left = `${x - 40}px`
+		// } else {
+		// 	e.style.left = `${x}px`
+		// }
+		// if (y > window.innerHeight) {
+		// 	e.style.top = `${y- 20}px`
+		// } else {
+		// 	e.style.top = `${y}px`
+		// }
+		e.style.top = Math.floor(Math.random()*90+5)+'%';
+		e.style.left = Math.floor(Math.random()*90+5)+'%';
+	 
 	}
 	return (
 		<button onMouseEnter={(e) => moveButton(e.target)} onClick={(e) => moveButton(e.target)} class="btn" id="noButton" onmouseover="moveButton()" onclick="moveButton()">No</button>			
